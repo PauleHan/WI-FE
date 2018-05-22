dc7fc1735a44ab1fe810b8c67d4826bc87c20dea
Paule HAN <han.paule@gmail.com>
2018-05-22 12:51:22 +0000

    Synthetic fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LongGridComponent } from './long-grid/long-grid.component';
+import { PostsBoardComponent } from './posts-board/posts-board.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
+  declarations: [
+    AppComponent,
+    LongGridComponent,
+    PostsBoardComponent,
+    SignInComponent
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
