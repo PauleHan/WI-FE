d6a13e35c8ab6dba087293784fa4cd88b66cbed1
Paule HAN <han.paule@gmail.com>
2018-05-22 12:42:55 +0000

    Added Routing: Main page, Login, Long-grid + param. US#219

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { AppComponent } from './app.component';
+import { LongGridComponent } from './long-grid/long-grid.component';
+import { PostsBoardComponent } from './posts-board/posts-board.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+
+const routes: Routes = [
+    {
+      path: '',
+      component: PostsBoardComponent
+    },
+    {
+      path: 'long-grid/:id',
+      component: LongGridComponent
+    },
+    {
+      path: 'login',
+      component: SignInComponent
+    }
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
