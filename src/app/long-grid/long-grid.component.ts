723c5ba75937600be18dda6252f2cdb0ea40996d
Paule HAN <han.paule@gmail.com>
2018-05-30 08:06:18 +0000

    Added ng-bootstrap

--- src/app/long-grid/long-grid.component.ts
+++ src/app/long-grid/long-grid.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-long-grid',
--- src/app/posts-board/posts-board.component.ts
+++ src/app/posts-board/posts-board.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-posts-board',
   templateUrl: './posts-board.component.html',
   styleUrls: ['./posts-board.component.scss']
 })
+
 export class PostsBoardComponent implements OnInit {
 
   constructor() { }
