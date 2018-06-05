90207757497e36df7e3cc2493ee46df872272062
Paule HAN <han.paule@gmail.com>
2018-06-05 07:39:41 +0000

    fixed import Auth service

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+//import { AuthService } from './auth.service';
+
 @NgModule({
   imports: [
     CommonModule
   ],
-  providers: [],
-  declarations: []
+  declarations: [
+    //AuthService
+  ],
+  providers: []
 })
 
 export class CoreModule { }
