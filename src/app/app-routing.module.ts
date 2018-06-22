99abf4d6c90cfb81e585dd718f389be7dc664e06
Paule HAN <han.paule@gmail.com>
2018-06-22 14:03:13 +0000

    Added new routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,28 +2,52 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { LongGridComponent } from './long-grid/long-grid.component';
-import { PostsBoardComponent } from './posts-board/posts-board.component';
-import { SignInComponent } from './sign-in/sign-in.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { ArticleDescComponent } from './articles/article-desc/article-desc.component';
+import { ArticleEditComponent } from './articles/article-edit/article-edit.component';
+import { SignInComponent } from './user/sign-in/sign-in.component';
+import { SignUpComponent } from './user/sign-up/sign-up.component';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
+import { SearchResultsComponent } from './pages/search-results/search-results.component';
 
 const routes: Routes = [
     {
-      path: '',
-      component: PostsBoardComponent
+        path: '',
+        component: ArticlesComponent
     },
     {
-      path: 'long-grid/:id',
-      component: LongGridComponent
+        path: 'long-grid/:id',
+        component: ArticleDescComponent
     },
     {
-      path: 'login',
-      component: SignInComponent
+        path: 'long-grid-edit/:id',
+        component: ArticleEditComponent
+    },
+    {
+        path: 'login',
+        component: SignInComponent
+    },
+    {
+        path: 'registration',
+        component: SignUpComponent
+    },
+    {
+        component: SearchResultsComponent,
+        path: 'search/:query',
+    },
+    {
+        path: '404',
+        component: NotFoundPageComponent,
+    },
+    {
+        path: '**',
+        redirectTo: '404' 
     }
-  ];
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 
 export class AppRoutingModule { }
