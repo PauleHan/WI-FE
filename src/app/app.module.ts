1cec5742c2d6af4d13ded04dae01dbdf5b46f432
Paule HAN <han.paule@gmail.com>
2018-06-23 21:22:18 +0000

    Update imports & declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,50 +1,57 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CollapseModule } from 'ngx-bootstrap';
+import { NgxEditorModule } from 'ngx-editor';
 
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core';
 import { AppComponent } from './app.component';
-import { LongGridComponent } from './long-grid/long-grid.component';
-import { PostsBoardComponent } from './posts-board/posts-board.component';
-import { SignInComponent } from './sign-in/sign-in.component';
-import { BlockSidebarComponent } from './block-sidebar/block-sidebar.component';
 import { HeaderComponent } from './header/header.component';
 import { ToolbarComponent } from './header/toolbar/toolbar.component';
 import { NavbarComponent } from './header/navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { SignInComponent } from './user/sign-in/sign-in.component';
+import { SignUpComponent } from './user/sign-up/sign-up.component';
 import { ArticlesComponent } from './articles/articles.component';
 import { ArticleItemComponent } from './articles/article-item/article-item.component';
 import { ArticleDescComponent } from './articles/article-desc/article-desc.component';
 import { ArticleEditComponent } from './articles/article-edit/article-edit.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
+import { SearchResultsComponent } from './pages/search-results/search-results.component';
+
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    CoreModule,
-    NgbModule.forRoot()
-  ],
-  declarations: [
-    AppComponent,
-    LongGridComponent,
-    PostsBoardComponent,
-    SignInComponent,
-    BlockSidebarComponent,
-    HeaderComponent,
-    ToolbarComponent,
-    NavbarComponent,
-    FooterComponent,
-    ArticlesComponent,
-    ArticleItemComponent,
-    ArticleDescComponent,
-    ArticleEditComponent,
-    SidebarComponent
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        FormsModule,
+        ReactiveFormsModule,
+        AppRoutingModule,
+        CollapseModule.forRoot(),
+        NgxEditorModule,
+        CoreModule
+    ],
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        ToolbarComponent,
+        NavbarComponent,
+        FooterComponent,
+        SignInComponent,
+        SignUpComponent,
+        ArticlesComponent,
+        ArticleItemComponent,
+        ArticleDescComponent,
+        ArticleEditComponent,
+        SidebarComponent,
+        NotFoundPageComponent,
+        SearchResultsComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 
 export class AppModule { }
