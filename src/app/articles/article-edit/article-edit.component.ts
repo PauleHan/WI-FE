1a8cb0fda68d9544e0353f4667d0f5020ae530b8
Paule HAN <han.paule@gmail.com>
2018-06-27 13:28:48 +0000

    Fix bug

--- src/app/articles/article-edit/article-edit.component.ts
+++ src/app/articles/article-edit/article-edit.component.ts
@@ -2,15 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-article-edit',
-  templateUrl: './article-edit.component.html',
-  styleUrls: ['./article-edit.component.scss']
+	selector: 'app-article-edit',
+	templateUrl: './article-edit.component.html',
+	styleUrls: ['./article-edit.component.scss']
 })
 export class ArticleEditComponent implements OnInit {
+	
+	htmlContent = 'Hello World';
+	
+	constructor() { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
 }
