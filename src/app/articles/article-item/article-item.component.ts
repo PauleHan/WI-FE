e993a0122cc62312dc2510fa86dd0ce85bb4e260
Paule HAN <han.paule@gmail.com>
2018-07-05 13:04:47 +0000

    Code optimize

--- src/app/articles/article-item/article-item.component.ts
+++ src/app/articles/article-item/article-item.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-article-item',
-  templateUrl: './article-item.component.html',
-  styleUrls: ['./article-item.component.scss']
+	selector: 'app-article-item',
+	templateUrl: './article-item.component.html',
+	styleUrls: ['./article-item.component.scss']
 })
 export class ArticleItemComponent implements OnInit {
+	
+	constructor() { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
 }
