8ea756ec8b79c8d3b051e2e4264d1038c26af19e
Paule HAN <han.paule@gmail.com>
2018-07-05 13:05:55 +0000

    Added subscribe

--- src/app/articles/article-desc/article-desc.component.ts
+++ src/app/articles/article-desc/article-desc.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+
+import { Article } from '../article.model';
+import { ArticleService } from './article.service';
 
 @Component({
-  selector: 'app-article-desc',
-  templateUrl: './article-desc.component.html',
-  styleUrls: ['./article-desc.component.scss']
+	selector: 'app-article-desc',
+	templateUrl: './article-desc.component.html',
+	styleUrls: ['./article-desc.component.scss']
 })
 export class ArticleDescComponent implements OnInit {
 
-  constructor() { }
+	article: Article;	
+	author: string;
+	permlink: string;
+	private sub: any;
 
-  ngOnInit() {
-  }
+	constructor(
+		private articleService: ArticleService,
+		private route: ActivatedRoute
+	) {}
 
+	ngOnInit() {
+		this.sub = this.route.params
+		.subscribe(
+			(params: Params) => {
+    			this.author		= params['author'];
+    			this.permlink	= params['permlink'];
+    		}
+    	);
+    	
+		this
+		.articleService
+		.getArticle(this.author, this.permlink)
+		.subscribe(
+			(data: Article) => {
+				this.article = data;
+			},
+			// (res) => console.log(res),
+			(error) => console.log(error)
+		);
+	}
+	
+	ngOnDestroy() {
+		this.sub.unsubscribe();
+	}
 }
