387982191f9fca65c468060de6f6246deac04e03
Paule HAN <han.paule@gmail.com>
2018-07-05 13:01:39 +0000

    Added servise provider

--- src/app/articles/articles.component.ts
+++ src/app/articles/articles.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Article } from './article.model';
+import { ArticlesService } from './articles.service';
 
 @Component({
-  selector: 'app-articles',
-  templateUrl: './articles.component.html',
-  styleUrls: ['./articles.component.scss']
+	selector: 'app-articles',
+	templateUrl: './articles.component.html',
+	styleUrls: ['./articles.component.scss']
 })
+
 export class ArticlesComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+	
+	articles: Article[] = [];
+	
+	constructor(private articlesService: ArticlesService) {}
+	
+	ngOnInit() {
+		this
+		.articlesService
+		.getArticles()
+		.subscribe(
+			(data: Article[]) => {
+				this.articles = data;
+			},
+			// (res) => console.log(res),
+			(error) => console.log(error)
+		);
+	}
 
 }
